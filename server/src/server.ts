b29acf875344d2fceba6bfdd78608df1849dab5c
Wick000 <chad.abramson987@gmail.com>
2025-03-05 16:41:17 -0500

    changed production server routes to process.cwd() for render deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -35,10 +35,10 @@ app.use('/graphql', expressMiddleware(server ,
 ));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(process.cwd(), '../client/dist')));
 
   app.get('*', (_req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    res.sendFile(path.join(process.cwd(), '../client/dist/index.html'));
   });
 }
 
